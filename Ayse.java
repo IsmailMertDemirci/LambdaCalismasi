eabefa49f03b3066ca243ab64fa5c1e6a5474f02
Semih Demir <joinko77@gmail.com>
2022-08-18 12:38:22 +0300

    selim

--- Ayse.java
+++ Ayse.java
@@ -4,5 +4,6 @@ public class Ayse {
     public static void main(String[] args) {
         System.out.println("Deneme1");
         System.out.println("Deneme2");
+        System.out.println("selim deneme");
     }
 }
